ab11cd255045a7e511929ad5620903374cce6955
ashcode1 <paypertiger@gmail.com>
2017-09-24 00:26:05 +0100

    [actions.test.js] {implements tests for fetchArticle}

--- src/__test__/actions.test.js
+++ src/__test__/actions.test.js
@@ -2,6 +2,7 @@ import * as actions from '../actions/actions';
 import * as fetchAllArticles from '../actions/fetchAllArticles';
 import * as fetchAllTopicTitles from '../actions/fetchAllTopicTitles';
 import * as fetchAllTopicArticles from '../actions/fetchAllTopicArticles';
+import * as fetchIndividualArticle from '../actions/fetchArticle';
 
 describe('fetchAllArticles', () => {
   test('it is a function', () => {
@@ -78,6 +79,33 @@ describe('fetchAllTopicArticles', () => {
   test('its error action returns data passed as a parameter', () => {
     const data = 'There was an Error';
     const test = fetchAllTopicArticles.fetchTopicArticlesError(data);
+    expect(test.data).toEqual('There was an Error');
+  });
+});
+
+describe('fetchArticle', () => {
+  test('it is a function', () => {
+    expect(typeof actions.fetchArticle).toBe('function');
+  });
+  test('its request action returns an object', () => {
+    expect(typeof fetchIndividualArticle.fetchArticleRequest()).toEqual('object');
+  });
+
+  test('its success action returns an object', () => {
+    expect(typeof fetchIndividualArticle.fetchArticleSuccess()).toEqual('object');
+  });
+  test('success action returns data passed as a parameter', () => {
+    const data = [1, 2, 3];
+    const test1 = fetchIndividualArticle.fetchArticleSuccess(data);
+    expect(test1.data).toEqual([1, 2, 3]);
+  });
+
+  test('its error action returns an object', () => {
+    expect(typeof fetchIndividualArticle.fetchArticleError()).toEqual('object');
+  });
+  test('its error action returns data passed as a parameter', () => {
+    const data = 'There was an Error';
+    const test = fetchIndividualArticle.fetchArticleError(data);
     expect(test.data).toEqual('There was an Error');
   });
 });
