ad8b38f462b940a7ee7466356f82ee2231c4a4e9
ashcode1 <paypertiger@gmail.com>
2017-09-23 22:31:44 +0100

    [App.js] {create route paths and basic layout}

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,36 @@
+/* 
+
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+import 'bulma/css/bulma.css';
+
+import Home from './Home';
+import TopicsHeader from './TopicsHeader';
+import TopicPage from './TopicPage';
+
+const history = createBrowserHistory();
 
 const App = () => (
-  <div>App</div>
+  <Router history={history}>
+    <div className="App">
+      <TopicsHeader />
+      <div className="columns" >
+        <div className="column" />
+        <div className="column is-half" />
+        <div className="column" />
+      </div>
+      <Route exact path="/" component={Home} />
+      <Route path="/articles/:topic_slug" component={TopicPage} />
+    </div>
+  </Router>
 );
 
+
+App.propTypes = {
+};
+
 export default App;
+
+*/
