ec93f5aebf3a3f70b7d7ba4b2c51f051fee8b9b6
ashcode1 <paypertiger@gmail.com>
2017-09-23 22:16:56 +0100

    [actions.test.js] {fetchAllTopicTitles - implement basic tests}

--- src/__test__/actions.test.js
+++ src/__test__/actions.test.js
@@ -1,5 +1,6 @@
 import * as actions from '../actions/actions';
 import * as fetchAllArticles from '../actions/fetchAllArticles';
+import * as fetchAllTopicTitles from '../actions/fetchAllTopicTitles';
 
 describe('fetchAllArticles', () => {
   test('it is a function', () => {
@@ -22,6 +23,33 @@ describe('fetchAllArticles', () => {
   test('its error action returns data passed as a parameter', () => {
     const data = 'There was an Error';
     const test = fetchAllArticles.fetchArticlesError(data);
+    expect(test.data).toEqual('There was an Error');
+  });
+});
+
+describe('fetchTopicTitles', () => {
+  test('it is a function', () => {
+    expect(typeof actions.fetchTopicTitles).toBe('function');
+  });
+  test('its request action returns an object', () => {
+    expect(typeof fetchAllTopicTitles.fetchTopicTitlesRequest()).toEqual('object');
+  });
+
+  test('its success action returns an object', () => {
+    expect(typeof fetchAllTopicTitles.fetchTopicTitlesSuccess()).toEqual('object');
+  });
+  test('success action returns data passed as a parameter', () => {
+    const data = [1, 2, 3];
+    const test1 = fetchAllTopicTitles.fetchTopicTitlesSuccess(data);
+    expect(test1.data).toEqual([1, 2, 3]);
+  });
+
+  test('its error action returns an object', () => {
+    expect(typeof fetchAllTopicTitles.fetchTopicTitlesError()).toEqual('object');
+  });
+  test('its error action returns data passed as a parameter', () => {
+    const data = 'There was an Error';
+    const test = fetchAllTopicTitles.fetchTopicTitlesError(data);
     expect(test.data).toEqual('There was an Error');
   });
 });
