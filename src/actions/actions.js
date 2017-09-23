3df788155308900ebd96a0e8cf75be1f5e5413fa
ashcode1 <paypertiger@gmail.com>
2017-09-23 20:44:09 +0100

    [actions.js] {includes fetchALlTopicArticles}

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import { ROOT } from '../config';
 import * as fetchAllArticles from './fetchAllArticles';
+import * as fetchAllTopicArticles from './fetchAllTopicArticles';
 
 // FETCH ALL ARTICLES
-function fetchArticles() {
+export function fetchArticles() {
   return (dispatch) => {
     dispatch(fetchAllArticles.fetchArticlesRequest());
     axios.get(`${ROOT}/articles`)
@@ -16,4 +17,16 @@ function fetchArticles() {
   };
 }
 
-export default fetchArticles;
+// FETCH TOPICS ARTICLES
+export function fetchTopicArticles(topicId) {
+  return (dispatch) => {
+    dispatch(fetchAllTopicArticles.fetchTopicArticlesRequest());
+    axios.get(`${ROOT}/topics/${topicId}/articles`)
+      .then((res) => {
+        dispatch(fetchAllTopicArticles.fetchTopicArticlesSuccess(res.data.articles));
+      })
+      .catch((err) => {
+        dispatch(fetchAllTopicArticles.fetchTopicArticlesError(err));
+      });
+  };
+}
