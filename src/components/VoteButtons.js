dbfe3c5a3d5927173823a054f2ed776d3132b28f
ashcode1 <paypertiger@gmail.com>
2017-10-01 17:38:11 +0100

    [VoteButtons.js] {change layout}

--- src/components/VoteButtons.js
+++ src/components/VoteButtons.js
@@ -3,26 +3,23 @@ import PropTypes from 'prop-types';
 import '../css/VoteButtons.css';
 
 const VoteButtons = props => (
-  <div className="column is-centred">
-    <a
-      className="button is-success is-small is-inverted"
-      onClick={props.handleVote.bind(null, props.id, 'up')}
-    >
-      <i className="fa fa-arrow-up row" />
-    </a>
-    <span className="row tag is-medium bold">{props.votes}</span>
-    <a
-      className="button is-danger is-small is-inverted"
-      onClick={props.handleVote.bind(null, props.id, 'down')}
-    >
-      <i className="fa fa-arrow-down row" />
-    </a>
-  </div>
+  <section className="voteSection">
+    <span className="column is-narrow rows">
+      <a className="is-danger is-small" onClick={props.handleVote.bind(null, props.id, 'up')} >
+        <i className="fa fa-arrow-circle-up row" />
+      </a>
+      <span className="row tag is-medium bold">{props.votes}</span>
+      <a className="is-danger is-small" onClick={props.handleVote.bind(null, props.id, 'down')} >
+        <i className="fa fa-arrow-circle-down row" />
+      </a>
+    </span>
+
+  </section>
 );
 
 VoteButtons.propTypes = {
-  id: PropTypes.string.isRequired,
   votes: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired,
   handleVote: PropTypes.func.isRequired,
 };
 
