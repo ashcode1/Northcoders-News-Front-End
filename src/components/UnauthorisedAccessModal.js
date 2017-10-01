54e2d9d803847aaeff860ca9df6e1c44c308aa44
ashcode1 <paypertiger@gmail.com>
2017-10-01 17:33:33 +0100

    [UnauthorisedAccessModal.js] {refactor}

--- src/components/UnauthorisedAccessModal.js
+++ src/components/UnauthorisedAccessModal.js
@@ -1,32 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class UnauthorisedAccessModal extends React.Component {
-  render() {
-    if (!this.props.modalOverlay) {
-      return null;
-    }
-    return (
-      <div className="modal is-active">
-        <div className="modal-background" />
-        <div className="modal-content">
-          <section className="box">
-            <div className="column is-8">
-              <h1 className="title"><b>Unauthorised Access</b></h1>
-              <p>
-                You do not have permission to delete other users comments.
-                You can however vote on other users' comments by clicking the up or down arrows.
-              </p>
-            </div>
-          </section>
-        </div>
-        <button className="modal-close is-large" aria-label="close" onClick={this.props.onClose} />
-      </div>
-    );
+const UnauthorisedUserModal = (props) => {
+  if (!props.modalOverlay) {
+    return null;
   }
-}
-
-UnauthorisedAccessModal.propTypes = {
-  modalOverlay: PropTypes.bool.isRequired,
+  return (
+    <div className="modal is-active">
+      <div className="modal-background" />
+      <div className="modal-content">
+        <section className="box">
+          <div className="column is-8">
+            <h1 className="title"><b>Unauthorised User!</b></h1>
+            <p>
+              You do not have permission to delete other users comments.
+              You can however vote on comments by clicking the up or down arrows.
+            </p>
+          </div>
+        </section>
+      </div>
+      <button className="modal-close is-large" aria-label="close" onClick={props.onClose} />
+    </div>
+  );
 };
-export default UnauthorisedAccessModal;
+
+UnauthorisedUserModal.propTypes = {
+  modalOverlay: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default UnauthorisedUserModal;
